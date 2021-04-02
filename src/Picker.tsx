b218cafdaf3ad09c9ce1fc1fe33d64278404fd98
umbrella <zhangqwesc@icloud.ocm>
2021-04-03 00:03:26 +0800

    add onItemClick callback

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -1,257 +1,255 @@
-import * as React from 'react';
-import classNames from 'classnames';
-import { IPickerProps } from './PickerTypes';
-import PickerMixin from './PickerMixin';
+import * as React from 'react'
+import classNames from 'classnames'
+import { IPickerProps } from './PickerTypes'
+import PickerMixin from './PickerMixin'
 
 export interface IPickerProp {
-  select: (...arg) => void;
-  doScrollingComplete: (...arg) => void;
-  computeChildIndex: (...arg) => number;
-};
+  select: (...arg) => void
+  doScrollingComplete: (...arg) => void
+  computeChildIndex: (...arg) => number
+}
 
 class Picker extends React.Component<IPickerProp & IPickerProps, any> {
   static defaultProps = {
     prefixCls: 'rmc-picker',
-  };
+  }
 
-  rootRef: any;
-  maskRef: any;
-  contentRef: any;
-  indicatorRef: any;
-  itemHeight: number;
-  scrollValue: any;
+  rootRef: any
+  maskRef: any
+  contentRef: any
+  indicatorRef: any
+  itemHeight: number
+  scrollValue: any
 
   scrollHanders = (() => {
-    let scrollY = -1;
-    let lastY = 0;
-    let startY = 0;
-    let scrollDisabled = false;
-    let isMoving = false;
+    let scrollY = -1
+    let lastY = 0
+    let startY = 0
+    let scrollDisabled = false
+    let isMoving = false
 
     const setTransform = (nodeStyle: CSSStyleDeclaration, value: any) => {
-      nodeStyle.transform = value;
-      nodeStyle.webkitTransform = value;
-    };
+      nodeStyle.transform = value
+      nodeStyle.webkitTransform = value
+    }
 
     const setTransition = (nodeStyle: CSSStyleDeclaration, value: any) => {
-      nodeStyle.transition = value;
-      nodeStyle.webkitTransition = value;
-    };
+      nodeStyle.transition = value
+      nodeStyle.webkitTransition = value
+    }
 
-    const scrollTo = (_x, y, time = .3) => {
+    const scrollTo = (_x, y, time = 0.3) => {
       if (scrollY !== y) {
-        scrollY = y;
+        scrollY = y
         if (time && !this.props.noAnimate) {
-          setTransition(this.contentRef.style, `cubic-bezier(0,0,0.2,1.15) ${time}s`);
+          setTransition(this.contentRef.style, `cubic-bezier(0,0,0.2,1.15) ${time}s`)
         }
-        setTransform(this.contentRef.style, `translate3d(0,${-y}px,0)`);
+        setTransform(this.contentRef.style, `translate3d(0,${-y}px,0)`)
         setTimeout(() => {
-          this.scrollingComplete();
+          this.scrollingComplete()
           if (this.contentRef) {
-            setTransition(this.contentRef.style, '');
+            setTransition(this.contentRef.style, '')
           }
-        }, +time * 1000);
+        }, +time * 1000)
       }
-    };
+    }
 
     const Velocity = ((minInterval = 30, maxInterval = 100) => {
-      let _time = 0;
-      let _y = 0;
-      let _velocity = 0;
+      let _time = 0
+      let _y = 0
+      let _velocity = 0
       const recorder = {
         record: (y) => {
-          const now = +new Date();
-          _velocity = (y - _y) / (now - _time);
+          const now = +new Date()
+          _velocity = (y - _y) / (now - _time)
           if (now - _time >= minInterval) {
-            _velocity = now - _time <= maxInterval ? _velocity : 0;
-            _y = y;
-            _time = now;
+            _velocity = now - _time <= maxInterval ? _velocity : 0
+            _y = y
+            _time = now
           }
         },
         getVelocity: (y) => {
           if (y !== _y) {
-            recorder.record(y);
+            recorder.record(y)
           }
-          return _velocity;
+          return _velocity
         },
-      };
-      return recorder;
-    })();
+      }
+      return recorder
+    })()
 
     const onFinish = () => {
-      isMoving = false;
-      let targetY = scrollY;
+      isMoving = false
+      let targetY = scrollY
 
-      const height = ((this.props.children as any).length - 1) * this.itemHeight;
+      const height = ((this.props.children as any).length - 1) * this.itemHeight
 
-      let time = .3;
+      let time = 0.3
 
-      const velocity = Velocity.getVelocity(targetY) * 4;
+      const velocity = Velocity.getVelocity(targetY) * 4
       if (velocity) {
-        targetY = velocity * 40 + targetY;
-        time = Math.abs(velocity) * .1;
+        targetY = velocity * 40 + targetY
+        time = Math.abs(velocity) * 0.1
       }
 
       if (targetY % this.itemHeight !== 0) {
-        targetY = Math.round(targetY / this.itemHeight) * this.itemHeight;
+        targetY = Math.round(targetY / this.itemHeight) * this.itemHeight
       }
 
       if (targetY < 0) {
-        targetY = 0;
+        targetY = 0
       } else if (targetY > height) {
-        targetY = height;
+        targetY = height
       }
 
-      scrollTo(0, targetY, time < .3 ? .3 : time);
-      this.onScrollChange();
-    };
+      scrollTo(0, targetY, time < 0.3 ? 0.3 : time)
+      this.onScrollChange()
+    }
 
     const onStart = (y: number) => {
       if (scrollDisabled) {
-        return;
+        return
       }
 
-      isMoving = true;
-      startY = y;
-      lastY = scrollY;
-    };
+      isMoving = true
+      startY = y
+      lastY = scrollY
+    }
 
     const onMove = (y: number) => {
       if (scrollDisabled || !isMoving) {
-        return;
+        return
       }
 
-      scrollY = lastY - y + startY;
-      Velocity.record(scrollY);
+      scrollY = lastY - y + startY
+      Velocity.record(scrollY)
 
-      this.onScrollChange();
-      setTransform(this.contentRef.style, `translate3d(0,${-scrollY}px,0)`);
-    };
+      this.onScrollChange()
+      setTransform(this.contentRef.style, `translate3d(0,${-scrollY}px,0)`)
+    }
 
     return {
       touchstart: (evt: React.TouchEvent<HTMLDivElement>) => onStart(evt.touches[0].pageY),
       mousedown: (evt: React.MouseEvent<HTMLDivElement>) => onStart(evt.pageY),
       touchmove: (evt: React.TouchEvent<HTMLDivElement>) => {
-        evt.preventDefault();
-        onMove(evt.touches[0].pageY);
+        evt.preventDefault()
+        onMove(evt.touches[0].pageY)
       },
       mousemove: (evt: React.MouseEvent<HTMLDivElement>) => {
-        evt.preventDefault();
-        onMove(evt.pageY);
+        evt.preventDefault()
+        onMove(evt.pageY)
       },
       touchend: () => onFinish(),
       touchcancel: () => onFinish(),
-      mouseup: () => onFinish(),
+      mouseup: (evt: React.MouseEvent<HTMLDivElement>) => this.onClick(evt.target as HTMLDivElement),
       getValue: () => {
-        return scrollY;
+        return scrollY
       },
       scrollTo,
       setDisabled: (disabled: boolean = false) => {
-        scrollDisabled = disabled;
+        scrollDisabled = disabled
       },
-    };
-  })();
+    }
+  })()
 
   constructor(props) {
-    super(props);
+    super(props)
 
-    let selectedValueState;
-    const { selectedValue, defaultSelectedValue } = this.props;
+    let selectedValueState
+    const { selectedValue, defaultSelectedValue } = this.props
     if (selectedValue !== undefined) {
-      selectedValueState = selectedValue;
+      selectedValueState = selectedValue
     } else if (defaultSelectedValue !== undefined) {
-      selectedValueState = defaultSelectedValue;
+      selectedValueState = defaultSelectedValue
     } else {
-      const children: any = React.Children.toArray(this.props.children);
-      selectedValueState = children && children[0] && children[0].props.value;
+      const children: any = React.Children.toArray(this.props.children)
+      selectedValueState = children && children[0] && children[0].props.value
     }
     this.state = {
       selectedValue: selectedValueState,
-    };
+    }
   }
 
   componentDidMount() {
-    const { contentRef, indicatorRef, maskRef, rootRef } = this;
-    const rootHeight = rootRef.getBoundingClientRect().height;
+    const { contentRef, indicatorRef, maskRef, rootRef } = this
+    const rootHeight = rootRef.getBoundingClientRect().height
     // https://github.com/react-component/m-picker/issues/18
-    const itemHeight = this.itemHeight = indicatorRef.getBoundingClientRect().height;
-    let num = Math.floor(rootHeight / itemHeight);
+    const itemHeight = (this.itemHeight = indicatorRef.getBoundingClientRect().height)
+    let num = Math.floor(rootHeight / itemHeight)
     if (num % 2 === 0) {
-      num--;
+      num--
     }
-    num--;
-    num /= 2;
-    contentRef.style.padding = `${itemHeight * num}px 0`;
-    indicatorRef.style.top = `${itemHeight * num}px`;
-    maskRef.style.backgroundSize = `100% ${itemHeight * num}px`;
-    this.scrollHanders.setDisabled(this.props.disabled);
-    this.props.select(this.state.selectedValue, this.itemHeight, this.scrollTo);
+    num--
+    num /= 2
+    contentRef.style.padding = `${itemHeight * num}px 0`
+    indicatorRef.style.top = `${itemHeight * num}px`
+    maskRef.style.backgroundSize = `100% ${itemHeight * num}px`
+    this.scrollHanders.setDisabled(this.props.disabled)
+    this.props.select(this.state.selectedValue, this.itemHeight, this.scrollTo)
 
-    const passiveSupported = this.passiveSupported();
-    const willPreventDefault = passiveSupported ? { passive: false } : false;
-    const willNotPreventDefault = passiveSupported ? { passive: true } : false;
-    Object.keys(this.scrollHanders).forEach(key => {
+    const passiveSupported = this.passiveSupported()
+    const willPreventDefault = passiveSupported ? { passive: false } : false
+    const willNotPreventDefault = passiveSupported ? { passive: true } : false
+    Object.keys(this.scrollHanders).forEach((key) => {
       if (key.indexOf('touch') === 0 || key.indexOf('mouse') === 0) {
-        const pd = key.indexOf('move') >= 0 ? willPreventDefault : willNotPreventDefault;
-        (rootRef as HTMLDivElement).addEventListener(key, this.scrollHanders[key], pd as any);
+        const pd = key.indexOf('move') >= 0 ? willPreventDefault : willNotPreventDefault
+        ;(rootRef as HTMLDivElement).addEventListener(key, this.scrollHanders[key], pd as any)
       }
-    });
+    })
   }
 
   componentWillUnmount() {
-    Object.keys(this.scrollHanders).forEach(key => {
+    Object.keys(this.scrollHanders).forEach((key) => {
       if (key.indexOf('touch') === 0 || key.indexOf('mouse') === 0) {
-        (this.rootRef as HTMLDivElement).removeEventListener(key, this.scrollHanders[key]);
+        ;(this.rootRef as HTMLDivElement).removeEventListener(key, this.scrollHanders[key])
       }
-    });
+    })
   }
 
   passiveSupported() {
-    let passiveSupported = false;
+    let passiveSupported = false
 
     try {
       const options = Object.defineProperty({}, 'passive', {
         get: () => {
-          passiveSupported = true;
+          passiveSupported = true
         },
-      });
-      window.addEventListener('test', null as any, options);
-    } catch (err) { }
-    return passiveSupported;
+      })
+      window.addEventListener('test', null as any, options)
+    } catch (err) {}
+    return passiveSupported
   }
 
   componentWillReceiveProps(nextProps: IPickerProp & IPickerProps) {
     if ('selectedValue' in nextProps) {
       if (this.state.selectedValue !== nextProps.selectedValue) {
-        this.setState({
-          selectedValue: nextProps.selectedValue,
-        }, () => {
-          this.props.select(
-            nextProps.selectedValue,
-            this.itemHeight,
-            nextProps.noAnimate ? this.scrollToWithoutAnimation : this.scrollTo,
-          );
-        });
+        this.setState(
+          {
+            selectedValue: nextProps.selectedValue,
+          },
+          () => {
+            this.props.select(nextProps.selectedValue, this.itemHeight, nextProps.noAnimate ? this.scrollToWithoutAnimation : this.scrollTo)
+          }
+        )
       }
     }
-    this.scrollHanders.setDisabled(nextProps.disabled);
+    this.scrollHanders.setDisabled(nextProps.disabled)
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return this.state.selectedValue !== nextState.selectedValue
-      || this.props.children !== nextProps.children;
+    return this.state.selectedValue !== nextState.selectedValue || this.props.children !== nextProps.children
   }
 
   componentDidUpdate() {
-    this.props.select(this.state.selectedValue, this.itemHeight, this.scrollToWithoutAnimation);
+    this.props.select(this.state.selectedValue, this.itemHeight, this.scrollToWithoutAnimation)
   }
 
   scrollTo = (top) => {
-    this.scrollHanders.scrollTo(0, top);
+    this.scrollHanders.scrollTo(0, top)
   }
 
   scrollToWithoutAnimation = (top) => {
-    this.scrollHanders.scrollTo(0, top, 0);
+    this.scrollHanders.scrollTo(0, top, 0)
   }
 
   fireValueChange = (selectedValue) => {
@@ -259,91 +257,95 @@ class Picker extends React.Component<IPickerProp & IPickerProps, any> {
       if (!('selectedValue' in this.props)) {
         this.setState({
           selectedValue,
-        });
+        })
       }
       if (this.props.onValueChange) {
-        this.props.onValueChange(selectedValue);
+        this.props.onValueChange(selectedValue)
       }
     }
   }
 
+  fireItemClick = (selectedValue) => {
+    if (selectedValue !== this.state.selectedValue) {
+      return
+    }
+    if (this.props.onItemClick) {
+      this.props.onItemClick(selectedValue)
+    }
+  }
+
   onScrollChange = () => {
-    const top = this.scrollHanders.getValue();
+    const top = this.scrollHanders.getValue()
     if (top >= 0) {
-      const children = React.Children.toArray(this.props.children);
-      const index = this.props.computeChildIndex(top, this.itemHeight, children.length);
+      const children = React.Children.toArray(this.props.children)
+      const index = this.props.computeChildIndex(top, this.itemHeight, children.length)
       if (this.scrollValue !== index) {
-        this.scrollValue = index;
-        const child: any = children[index];
+        this.scrollValue = index
+        const child: any = children[index]
         if (child && this.props.onScrollChange) {
-          this.props.onScrollChange(child.props.value);
+          this.props.onScrollChange(child.props.value)
         } else if (!child && console.warn) {
-          console.warn('child not found', children, index);
+          console.warn('child not found', children, index)
         }
       }
     }
   }
 
   scrollingComplete = () => {
-    const top = this.scrollHanders.getValue();
+    const top = this.scrollHanders.getValue()
     if (top >= 0) {
-      this.props.doScrollingComplete(top, this.itemHeight, this.fireValueChange);
+      this.props.doScrollingComplete(top, this.itemHeight, this.fireValueChange)
     }
+  }
+
+  onClick = (target: HTMLDivElement) => {
+    this.fireItemClick(target.getAttribute('data-value'))
   }
 
   getValue() {
     if ('selectedValue' in this.props) {
-      return this.props.selectedValue;
+      return this.props.selectedValue
     }
-    const children: any = React.Children.toArray(this.props.children);
-    return children && children[0] && children[0].props.value;
+    const children: any = React.Children.toArray(this.props.children)
+    return children && children[0] && children[0].props.value
   }
 
   render() {
-    const { props } = this;
-    const {
-      prefixCls,
-      itemStyle,
-      indicatorStyle,
-      indicatorClassName = '',
-      children,
-    } = props;
-    const { selectedValue } = this.state;
-    const itemClassName = `${prefixCls}-item`;
-    const selectedItemClassName = `${itemClassName} ${prefixCls}-item-selected`;
+    const { props } = this
+    const { prefixCls, itemStyle, indicatorStyle, indicatorClassName = '', children } = props
+    const { selectedValue } = this.state
+    const itemClassName = `${prefixCls}-item`
+    const selectedItemClassName = `${itemClassName} ${prefixCls}-item-selected`
     const map = (item: any) => {
-      const { className = '', style, value } = item.props;
+      const { className = '', style, value } = item.props
       return (
         <div
           style={{ ...itemStyle, ...style }}
           className={`${selectedValue === value ? selectedItemClassName : itemClassName} ${className}`}
           key={value}
+          data-value={value}
         >
           {item.children || item.props.children}
         </div>
-      );
-    };
+      )
+    }
     // compatibility for preact
-    const items = React.Children ? React.Children.map(children, map) : ([] as any[]).concat(children).map(map);
+    const items = React.Children ? React.Children.map(children, map) : ([] as any[]).concat(children).map(map)
 
     const pickerCls = {
       [props.className as string]: !!props.className,
       [prefixCls as string]: true,
-    };
+    }
     return (
-      <div className={classNames(pickerCls)} ref={el => this.rootRef = el} style={this.props.style}>
-        <div className={`${prefixCls}-mask`} ref={el => this.maskRef = el} />
-        <div
-          className={`${prefixCls}-indicator ${indicatorClassName}`}
-          ref={el => this.indicatorRef = el}
-          style={indicatorStyle}
-        />
-        <div className={`${prefixCls}-content`} ref={el => this.contentRef = el}>
+      <div className={classNames(pickerCls)} ref={(el) => (this.rootRef = el)} style={this.props.style}>
+        <div className={`${prefixCls}-mask`} ref={(el) => (this.maskRef = el)} />
+        <div className={`${prefixCls}-indicator ${indicatorClassName}`} ref={(el) => (this.indicatorRef = el)} style={indicatorStyle} />
+        <div className={`${prefixCls}-content`} ref={(el) => (this.contentRef = el)}>
           {items}
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default PickerMixin(Picker);
+export default PickerMixin(Picker)
--- src/PickerTypes.tsx
+++ src/PickerTypes.tsx
@@ -1,15 +1,16 @@
 export interface IPickerProps {
-  disabled?: boolean;
-  selectedValue?: any;
-  onValueChange?: (value: any) => void;
-  itemStyle?: any;
+  disabled?: boolean
+  selectedValue?: any
+  onValueChange?: (value: any) => void
+  itemStyle?: any
   /** web only */
-  prefixCls?: string;
-  indicatorStyle?: any;
-  indicatorClassName?: string;
-  className?: string;
-  defaultSelectedValue?: any;
-  style?: any;
-  onScrollChange?: (value: any) => void;
-  noAnimate?: boolean;
-};
+  prefixCls?: string
+  indicatorStyle?: any
+  indicatorClassName?: string
+  className?: string
+  defaultSelectedValue?: any
+  style?: any
+  onScrollChange?: (value: any) => void
+  noAnimate?: boolean
+  onItemClick?: (value: any) => void
+}
